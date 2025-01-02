4afe19d8174acb27205a0a02d67db217dbf321cd
ubednama <shahidkhan.ak18@gmail.com>
2025-01-02 12:53:06 +0530

    Add safelist for hover classes in Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    'hover:bg-violet-500',
+    'hover:none',
+    // Add other hover classes if needed
+  ],
   theme: {
     extend: {
       // fontFamily: {
