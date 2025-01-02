9885237dab4c97214d9d4cd213fadf3e0b3dcf12
ubednama <shahidkhan.ak18@gmail.com>
2025-01-02 13:14:36 +0530

    Refactor Work component to use useEffect for image rotation and update image URLs

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { motion, useScroll } from "framer-motion";
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
 const Work = () => {
   const [images, setImages] = useState([
@@ -10,15 +10,15 @@ const Work = () => {
       isActive: true,
     },
     {
-      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0acbc45cb2f4fc5c6b2_Yahoo.png",
-      top: "45%",
-      left: "56%",
+      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0accfe1b3e66bc55462_Refokus%20Tools.png",
+      top: "56%",
+      left: "44%",
       isActive: false,
     },
     {
-      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0ac7e7179d210dc41f0_Summon.png",
-      top: "49%",
-      left: "45%",
+      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0acbc45cb2f4fc5c6b2_Yahoo.png",
+      top: "45%",
+      left: "56%",
       isActive: false,
     },
     {
@@ -28,9 +28,9 @@ const Work = () => {
       isActive: false,
     },
     {
-      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0accfe1b3e66bc55462_Refokus%20Tools.png",
-      top: "56%",
-      left: "44%",
+      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0ac7e7179d210dc41f0_Summon.png",
+      top: "43%",
+      left: "40%",
       isActive: false,
     },
     {
@@ -41,43 +41,20 @@ const Work = () => {
     },
   ]);
 
-  const { scrollYProgress } = useScroll();
-
-  scrollYProgress.on("change", (data) => {
-    function imagesShow(arr) {
-      setImages((prev) =>
-        prev.map((item, index) =>
-          arr.indexOf(index) === -1
-            ? { ...item, isActive: false }
-            : { ...item, isActive: true }
-        )
+  useEffect(() => {
+    let currentIndex = 0;
+    const interval = setInterval(() => {
+      setImages((prevImages) =>
+        prevImages.map((image, index) => ({
+          ...image,
+          isActive: index === currentIndex,
+        }))
       );
-    }
+      currentIndex = (currentIndex + 1) % images.length;
+    }, 2000);
 
-    switch (Math.floor(data * 100)) {
-      case 0:
-        imagesShow([]);
-        break;
-      case 1:
-        imagesShow([0]);
-        break;
-      case 4:
-        imagesShow([0, 1]);
-        break;
-      case 6:
-        imagesShow([0, 1, 2]);
-        break;
-      case 9:
-        imagesShow([0, 1, 2, 3]);
-        break;
-      case 12:
-        imagesShow([0, 1, 2, 3, 4]);
-        break;
-      case 15:
-        imagesShow([0, 1, 2, 3, 4, 5]);
-        break;
-    }
-  });
+    return () => clearInterval(interval);
+  }, [images.length]);
 
   return (
     <div className="w-full mt-20">
@@ -91,13 +68,13 @@ const Work = () => {
           (elem, index) =>
             elem.isActive && (
               <motion.img
+                key={index}
                 className="absolute w-60 rounded-lg -translate-x-[50%] -translate-y-[50%]"
                 src={elem.url}
-                key={index}
                 style={{ top: elem.top, left: elem.left }}
                 alt="work"
                 initial={{ scale: 0 }}
-                animate={{ scale: 1 }}
+                animate={{ scale: elem.isActive ? 1 : 0 }}
                 transition={{ duration: 0.5 }}
               />
             )
